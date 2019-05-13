18620b2c04ac4529ad323ae156b33510b7fd236f
Kamil <kmirosz6@gmail.com>
2019-05-13 14:53:08 +0200

    create font style

--- stories/texts.stories.js
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/html';
-
-import '../resources/assets/styles/main.scss';
-
-storiesOf('Texts', module)
-  .add('Headline', () => '<h1 class="headline">Headline</h1>')
-  .add('Title', () => '<h2 class="title">Headline</h1>');
