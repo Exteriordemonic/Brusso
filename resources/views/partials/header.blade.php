6c07c24394d0216f70af0dc611561bd882734121
Kamil Mirosz <kmirosz6@gmail.com>
2019-06-19 18:00:46 +0200

    header with logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
-@php    
+@php
   $phone = get_field('phone','options');
-  $mail = get_field('mail','options'); 
+  $mail = get_field('mail','options');
 
   $sm = get_field('icons');
 @endphp
@@ -9,19 +9,10 @@
   <div class="container">
     <div class="row">
       <div class="col header__wrapper">
-        {{-- <a class="header__brand" href="{{ home_url('/') }}">
-          <img src="{{  get_field('logo', 'options')['url'] }}" alt="Jartech">
+        <a class="header__brand" href="{{ home_url('/') }}">
+          <img src="@asset('/images/logo.png')" alt="Brusso">
         </a>
-        <button class="header__hamburger hamburger" data-toggle-menu>
-          <span class="hamburger__line"></span>
-          <span class="hamburger__line"></span>
-          <span class="hamburger__line"></span>
-        </button>
-        <nav class="header__nav" data-nav>
-          @if (has_nav_menu('primary_navigation'))
-            {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'header__menu']) !!}
-          @endif
-        </nav> --}}
+        </nav>
       </div>
     </div>
   </div>
