a355ddbfb798d38e3317f0ccb73e12ec2a47f95b
Kamil Mirosz <kmirosz6@gmail.com>
2019-08-07 16:35:25 +0200

    fix ajax add to basket after load color

--- resources/assets/scripts/woocommerce/ajax-single-product-color.js
+++ resources/assets/scripts/woocommerce/ajax-single-product-color.js
@@ -2,6 +2,7 @@ import 'jquery';
 import priceFormt from './price-format'
 import shopColor from './shop-color'
 import slider from '../components/slider'
+import addToCart from './add-to-cart'
 
 const CONFIG = {
     TRIGGER: '[data-color-single-product]',
@@ -55,6 +56,7 @@ const ajaxColor = {
             shopColor.init();
             ajaxColor.init();
             slider.init();
+            addToCart.init();
 
             $(this).removeClass(CLASS.loading);
         });
